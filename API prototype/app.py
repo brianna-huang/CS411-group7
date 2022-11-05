4e7967d6d9d8f5d7e9808914e60ccb3b76f48004
briannahuang <brihuang@bu.edu>
2022-11-05 12:51:26 -0400

    added note about installation

--- API prototype/app.py
+++ API prototype/app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 import urllib.request, json
 import requests
 
+# Enter following into terminal to install framework & libraries:
+# python3 -m venv venv
+# source venv/bin/activate
+# pip install flask
+# pip install requests
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,6 +21,7 @@ def get_ingredients():
         "X-RapidAPI-Key": "a4e2fc244bmshd6877fd389df594p162eb8jsn5b123867a7d7",
         "X-RapidAPI-Host": "mycookbook-io1.p.rapidapi.com"
     }
+    # API subscription is not complete, so the requests won't go through yet
     response = requests.request("POST", url, data=payload, headers=headers)
     parse_response = json.loads(response)
     return(parse_response["ingredients"])
