f9049bd7bbaa05b07dbb15f594e5922972fa83dc
brianna-huang <107503941+brianna-huang@users.noreply.github.com>
2022-12-06 16:42:47 -0500

    removed incorrect signup code

--- prototype/app.py
+++ prototype/app.py
@@ -107,36 +107,6 @@ def get_target_products(ingredient):
         return_string = return_string + item + ":<br/>" + "price:" + return_dict[item][1] + "<br/>" + "url: " + return_dict[item][0] + "<br/>"
     return return_string
 
-@app.route('/signup', methods=['GET','POST'])
-def register():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQL.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
-    elif request.method == 'POST':
-        # Form is empty... (no POST data)
-        msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
-    return render_template('index.html', msg=msg)
-
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
